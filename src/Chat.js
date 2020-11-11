2548e186c532043b051db8d3a8db8d7835f5b6c4
bosshogg86 <jeffhogg86@gmail.com>
2020-11-10 22:55:24 -0800

    adds chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,39 @@
+import {
+  AddCircle,
+  CardGiftcard,
+  EmojiEmotions,
+  Gif,
+} from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
+import ChatHeader from "./ChatHeader";
+import Message from "./Message";
 
 function Chat() {
   return (
     <div className="chat">
-      <h2>Chat Screen</h2>
+      <ChatHeader />
+      <div className="chat__messages">
+        <Message />
+        <Message />
+        <Message />
+      </div>
+
+      <div className="chat__input">
+        <AddCircle fontSize="large" />
+        <form>
+          <input placeholder={`Message #TESTCHANNEL`} />
+          <button className="chat__inputButton" type="submit">
+            Send Message
+          </button>
+        </form>
+
+        <div className="chat__inputIcons">
+          <CardGiftcard fontSize="large" />
+          <Gif fontSize="large" />
+          <EmojiEmotions fontSize="large" />
+        </div>
+      </div>
     </div>
   );
 }
